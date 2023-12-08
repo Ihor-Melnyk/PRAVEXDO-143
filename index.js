3f0021aaccfefe381ba7e54fe4d0f246e015380a
Ihor Melnyk <igor.melnick85@gmail.com>
2023-12-08 15:31:04 +0200

    Changed function setDataForESign

---
+++ index.js
@@ -0,0 +1,64 @@
+function setDataForESign() {
+  var regDate = EdocsApi.getAttributeValue("RegDate").value;
+  var regNumber = EdocsApi.getAttributeValue("RegNumber").value;
+  var caseType = EdocsApi.getAttributeValue("TypeDoc").value;
+  var caseKind = EdocsApi.getAttributeValue("ContractType").text;
+  var name = "";
+  if (caseKind) {
+    name += caseKind;
+  } else {
+    name += caseType;
+  }
+
+  var name = (regNumber ? regNumber : CurrentDocument.id) + (!regDate ? "" : " від " + moment(regDate).format("DD.MM.YYYY"));
+
+  var doc = {
+    DocumentName: name,
+    extSysDocId: CurrentDocument.id,
+    ExtSysDocVersion: CurrentDocument.version,
+    DocumentType: "act",
+    docDate: registrationDate,
+    docNum: registrationNumber,
+    File: "",
+    parties: [
+      {
+        taskType: "ToSign",
+        taskState: "Done",
+        legalEntityCode: EdocsApi.getAttributeValue("OrgCode").value,
+        contactPersonEmail: EdocsApi.getAttributeValue("OrgContractManagerEmail").value,
+        signatures: [],
+      },
+      {
+        taskType: "ToSign",
+        taskState: "NotAssigned",
+        legalEntityCode: EdocsApi.getAttributeValue("ContractorCode").value,
+        contactPersonEmail: EdocsApi.getAttributeValue("CtrpContractManagerEmail").value,
+        expectedSignatures: [
+          {
+            SignerTitle: EdocsApi.getAttributeValue("ContractorRPSurname").value,
+            //Position: атрибут із посадою підписанта, EdocsApi.getAttributeValue("ContractorAgentPosition").value,
+          },
+        ],
+      },
+    ],
+    additionalAttributes: [
+      {
+        code: "docDate",
+        type: "dateTime",
+        value: registrationDate,
+      },
+      {
+        code: "docNum",
+        type: "string",
+        value: registrationNumber,
+      },
+    ],
+    sendingSettings: {
+      attachFiles: "fixed", //, можна також встановлювати 'firstOnly' - Лише файл із першої зафіксованої вкладки(Головний файл), або 'all' - всі файли, 'fixed' - усі зафіксовані
+      attachSignatures: "signatureAndStamp", // -'signatureAndStamp'Типи “Підпис” або “Печатка”, можна також встановити 'all' - усі типи цифрових підписів
+    },
+  };
+  if (EdocsApi.getAttributeValue("Vchasno").value == "true") doc.parties[1].Task.sendByService = "vchasno";
+
+  EdocsApi.setAttributeValue({ code: "JSON", value: JSON.stringify(doc) });
+}
